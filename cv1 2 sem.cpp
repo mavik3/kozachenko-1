bd0bc770d1512cbb9a86eaa93e871be6ba4eb7ab
mavik3 <sasakozacenko885@gmail.com>
2025-02-26 22:57:19 +0100

    1

---
+++ cv1 2 sem.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+
+
+int main(){
+	int m[]={1,2,34,5,4,7};
+	int d[]={1,5,7,4,42,6,78};
+	
+
+	for(int i=0;i<6;i++){
+		for(int j=0;j<7;i++){
+			if(m[i]==d[j]){
+				printf("%d ",m[i]);
+				j++;
+				m[i]=d[j];
+			}
+		}
+	}
+printf("\n");
+	for(int i=0;i<6;i++){
+		for(int j=0;j<7;i++){
+			if()
+			}
+}
+}
+}
